9439e3b2fb90290c33e77e88431c11da4bf916a3
TuinderJ <100104394+TuinderJ@users.noreply.github.com>
2024-02-09 00:03:13 -0700

    made some changes

---
+++ utilities/hurt_box.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if not body is Player:
+		return
+
+	get_parent().take_damage()
